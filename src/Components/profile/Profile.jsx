649009640be9f8e8a76ef8eed73f2a8389cc1892
neoTemores <neo.temores@live.com>
2023-04-03 17:59:56 -0700

    css for comments

--- src/Components/profile/Profile.jsx
+++ src/Components/profile/Profile.jsx
@@ -22,24 +22,33 @@ const Profile = () => {
 
     return (
         <div className='profilePageContainer'>
-            <div>
+            <div className='myProfileMyPostsContainer'>
                 <h1>Posts</h1>
                 {userProfile?.userPosts?.posts?.slice(0, 3).map(elem => {
                     return (
                         <div key={elem.id} className='individualPostContainer'>
                             <h3>{elem.title}</h3>
                             <p>{elem.body}</p>
+                            <div className='postEditDeleteUpdateCancelBtnContainer myPosts'>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </div>
                         </div>
                     )
                 })}
                 {userProfile?.userPosts?.posts?.length > 3 && <button>View all posts</button>}
             </div>
-            <div>
+
+            <div className='myProfileMyCommentsContainer'>
                 <h1>Comments</h1>
                 {userProfile?.userComments?.commentList?.slice(0, 3).map(elem => {
                     return (
                         <div key={elem.id} className='individualPostContainer'>
                             <p>{elem.body}</p>
+                            <div className='commentBtnsContainer myComments'>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </div>
                         </div>
                     )
                 })}
