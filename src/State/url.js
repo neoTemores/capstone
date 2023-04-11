dadcfa33f351b87a6555f4555ebd4b078139a297
neoTemores <neo.temores@live.com>
2023-04-11 12:00:46 -0700

    deploy css ham menu and sort wallet

--- src/State/url.js
+++ src/State/url.js
@@ -1,87 +1,88 @@
-// export const COIN_URL = {
-//     "LIST_NAME": "coinList",
-//     "GET_ALL": "/api/get-coins",
-//     "GET_ONE": "/api/coin/"
-// }
-
-// export const POSTS_URL = {
-//     "LIST_NAME": "posts",
-//     "GET_ALL": "/api/posts",
-//     "GET_ONE": "/api/posts/",
-//     "POST": "/api/posts",
-//     "PATCH": "/api/posts/",
-//     "DELETE": "/api/posts/",
-// }
-
-// export const WALLET_URL = {
-//     "LIST_NAME": "savedCoinsList",
-//     "GET_ALL_BY_USER": "/api/saved-coins/user/",
-//     "POST": "/api/saved-coins/add",
-//     "DELETE": "/api/saved-coins/delete/"
-// }
-
-// export const COMMENTS_URL = {
-//     "LIST_NAME": "commentList",
-//     "GET_ALL": "/api/comments",
-//     "GET_ALL_BY_POST_ID": "/api/comments/post/",
-//     "POST": "/api/comments/add",
-//     "PATCH": "/api/comments/edit/",
-//     "DELETE": "/api/comments/delete/",
-//     "DELETE_BY_POST_ID": "/api/comments/delete-all/post/"
-// }
-
-// export const USER_URL = {
-//     "LOGIN": "/api/getuser",
-//     "CREATE": "/api/createuser",
-//     "UPDATE": "/api/updateuser/",
-//     "ALL_USERS": "/api/users",
-//     "DELETE": "/api/deleteuser/",
-// }
-
-// export const USER_PROFILE_URL = {
-//     "GET": "/api/"
-// }
-
 export const COIN_URL = {
     "LIST_NAME": "coinList",
-    "GET_ALL": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/get-coins",
-    "GET_ONE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/coin/"
+    "GET_ALL": "/api/get-coins",
+    "GET_ONE": "/api/coin/"
 }
 
 export const POSTS_URL = {
     "LIST_NAME": "posts",
-    "GET_ALL": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/posts",
-    "GET_ONE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/posts/",
-    "POST": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/posts",
-    "PATCH": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/posts/",
-    "DELETE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/posts/",
+    "GET_ALL": "/api/posts",
+    "GET_ONE": "/api/posts/",
+    "POST": "/api/posts",
+    "PATCH": "/api/posts/",
+    "DELETE": "/api/posts/",
 }
 
 export const WALLET_URL = {
     "LIST_NAME": "savedCoinsList",
-    "GET_ALL_BY_USER": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/saved-coins/user/",
-    "POST": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/saved-coins/add",
-    "DELETE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/saved-coins/delete/"
+    "GET_ALL_BY_USER": "/api/saved-coins/user/",
+    "POST": "/api/saved-coins/add",
+    "DELETE": "/api/saved-coins/delete/"
 }
 
 export const COMMENTS_URL = {
     "LIST_NAME": "commentList",
-    "GET_ALL": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments",
-    "GET_ALL_BY_POST_ID": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments/post/",
-    "POST": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments/add",
-    "PATCH": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments/edit/",
-    "DELETE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments/delete/",
-    "DELETE_BY_POST_ID": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments/delete-all/post/"
+    "GET_ALL": "/api/comments",
+    "GET_ALL_BY_POST_ID": "/api/comments/post/",
+    "POST": "/api/comments/add",
+    "PATCH": "/api/comments/edit/",
+    "DELETE": "/api/comments/delete/",
+    "DELETE_BY_POST_ID": "/api/comments/delete-all/post/"
 }
 
 export const USER_URL = {
-    "LOGIN": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/getuser",
-    "CREATE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/createuser",
-    "UPDATE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/updateuser/",
-    "ALL_USERS": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/users",
-    "DELETE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/deleteuser/",
+    "LOGIN": "/api/getuser",
+    "CREATE": "/api/createuser",
+    "UPDATE": "/api/updateuser/",
+    "ALL_USERS": "/api/users",
+    "DELETE": "/api/deleteuser/",
 }
 
 export const USER_PROFILE_URL = {
-    "GET": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/"
+    "GET": "/api/"
 }
+
+
+// export const COIN_URL = {
+//     "LIST_NAME": "coinList",
+//     "GET_ALL": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/get-coins",
+//     "GET_ONE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/coin/"
+// }
+
+// export const POSTS_URL = {
+//     "LIST_NAME": "posts",
+//     "GET_ALL": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/posts",
+//     "GET_ONE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/posts/",
+//     "POST": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/posts",
+//     "PATCH": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/posts/",
+//     "DELETE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/posts/",
+// }
+
+// export const WALLET_URL = {
+//     "LIST_NAME": "savedCoinsList",
+//     "GET_ALL_BY_USER": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/saved-coins/user/",
+//     "POST": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/saved-coins/add",
+//     "DELETE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/saved-coins/delete/"
+// }
+
+// export const COMMENTS_URL = {
+//     "LIST_NAME": "commentList",
+//     "GET_ALL": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments",
+//     "GET_ALL_BY_POST_ID": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments/post/",
+//     "POST": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments/add",
+//     "PATCH": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments/edit/",
+//     "DELETE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments/delete/",
+//     "DELETE_BY_POST_ID": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/comments/delete-all/post/"
+// }
+
+// export const USER_URL = {
+//     "LOGIN": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/getuser",
+//     "CREATE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/createuser",
+//     "UPDATE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/updateuser/",
+//     "ALL_USERS": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/users",
+//     "DELETE": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/deleteuser/",
+// }
+
+// export const USER_PROFILE_URL = {
+//     "GET": "http://eaglewallet-env.eba-q3b6jzqz.us-east-2.elasticbeanstalk.com/api/"
+// }
